c72bce7f26529483ae2185e335db2cf3743432b9
donvadicastro <vadim.barilo@gmail.com>
2023-02-07 19:17:58 +0200

    1 binaries uploaded

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
+import * as React from 'react';
 
 export const Page: React.FC<any> = () => {
   return (
     <div className="c-portal">
-      <h2>Users page</h1>
+      <h2>Users page</h2>
       <div className="js-portal-custom-widget">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid veniam distinctio corrupti quasi consequuntur illo alias id omnis illum, dolor voluptatem accusamus? Veniam nostrum cumque sit illum, vel odit debitis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid veniam
+        distinctio corrupti quasi consequuntur illo alias id omnis illum, dolor
+        voluptatem accusamus? Veniam nostrum cumque sit illum, vel odit debitis?
       </div>
     </div>
   );
